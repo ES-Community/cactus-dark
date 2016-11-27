674d2df0fcd5e0cc35e22155aec590b459b90984
Purexo <theotime93@yahoo.fr>
2016-11-27 18:12:53 +0100

    add disqus support

---
+++ layout/_partial/git-history.js
@@ -0,0 +1,3 @@
+<a href="https://github.com/ES-Community/ES-Community.github.io/blame/source/source/<%= page.source %>" target="_blank">Blame</a> - 
+<a href="https://github.com/ES-Community/ES-Community.github.io/commits/source/source/<%= page.source %>" target="_blank">History</a>
+  
