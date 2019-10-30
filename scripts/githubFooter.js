2debac5299d6f5c1d0009d2a0bdbbf88486a11e8
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2019-10-30 17:34:44 +0100

    fix: replace hardcode link to config link

--- scripts/githubFooter.js
+++ scripts/githubFooter.js
@@ -1,4 +1,5 @@
-const repos = 'https://github.com/ES-Community/ES-Community.github.io'
+// Get the deploy repo to use it in github query
+const repos = hexo.config.deploy.repo
 
 hexo.extend.helper.register('githubBlame', ({ url }) => `${repos}/blame/source/source/${url}`)
 hexo.extend.helper.register('githubCommits', ({ url }) => `${repos}/commits/source/source/${url}`)
